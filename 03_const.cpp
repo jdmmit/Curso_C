426cb5cbb71afff62c3170da1cb872db132c0db5
jdmmit <juandavidmurillomejia0718@outlook.es>
2025-08-16 22:59:25 -0500

    Actualizacion

---
+++ 03_const.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    double pi = 3.14159;
+    double radio = 10;
+    double circunferecia = 2 * pi * radio;
+
+    std::cout << circunferecia << " Centimetros";
+
+    return 0;
+}
---
+++ 04_Namespace.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+namespace primero
+{
+    int x = 1;
+} // namespace primero
+
+namespace segundo
+{
+    int x = 2;
+}
+
+int main()
+{
+
+    int x = 0;
+    std::cout << "x = " << x;
+
+    return 0;
+}
